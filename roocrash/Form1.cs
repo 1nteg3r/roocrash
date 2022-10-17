5e6784f658bb2c60301d484ad9ac87d2b243db7c
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2022-10-17 21:12:43 +0200

    dodadena databaza

--- roocrash/Form1.cs
+++ roocrash/Form1.cs
@@ -5,14 +5,17 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace roocrash
 {
     public partial class Form1 : Form
     {
+        bool eb = false;
         bool mouseDown;
         private Point offset;
         public Form1()
@@ -86,9 +89,20 @@ namespace roocrash
             Process.Start("https://discord.gg/YYTYt9JB");
         }
 
+        
         private void guna2GradientButton1_Click(object sender, EventArgs e)
         {
-
+            string paste = new System.Net.WebClient() { Proxy = null }.DownloadString("https://pastebin.com/raw/5bEc5awz");
+            if (paste.Contains(guna2TextBox1.Text) && guna2TextBox1.Text != "")
+            {
+                eb = true;
+                MessageBox.Show("Correct key!");
+            }
+            else
+            {
+                eb =false;
+                MessageBox.Show("Invalid key, please contact tefan#7872 on discord ");
+            }
         }
     }
 }
